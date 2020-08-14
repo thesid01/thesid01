45d98066a8eb450035a3a11e93e18c9467348d37
Siddharth <sidkv.skv@gmail.com>
2020-08-14 22:16:10 +0530

    Updates

---
+++ precache-manifest.4eaa9565ecdf9907adba3bebe789ecee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5536bd8acef002d228f04cbd22836f20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "987a5649d5535018829d",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "f1aef30cb7b7f36c082c",
+    "url": "/static/css/main.1ff9862d.chunk.css"
+  },
+  {
+    "revision": "987a5649d5535018829d",
+    "url": "/static/js/2.958e8d3c.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.958e8d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1aef30cb7b7f36c082c",
+    "url": "/static/js/main.d2f6da65.chunk.js"
+  },
+  {
+    "revision": "586c5f9b098255b7d57b",
+    "url": "/static/js/runtime-main.b5cd84d2.js"
+  },
+  {
+    "revision": "12f22062324aa1687c5890bdcae5d6b3",
+    "url": "/static/media/IMG.12f22062.JPG"
+  }
+]);
